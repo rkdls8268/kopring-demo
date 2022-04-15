b50a9dfd6062cc41a6558099056200375247f98a
Jerry <jerry@nextunicorn.kr>
2022-04-15 15:28:11 +0900

    first commit

---
+++ src/main/kotlin/com/example/nextu/NextuApplication.kt
@@ -0,0 +1,11 @@
+package com.example.nextu
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NextuApplication
+
+fun main(args: Array<String>) {
+	runApplication<NextuApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/nextu/NextuApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.nextu
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NextuApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
