669d50eaf39f871f2c2bfe1602ec26f67f0595dc
rkdls8268 <rkdls8268@gmail.com>
2023-06-25 16:11:23 +0900

    [FEAT] swagger jwt 전역 인증 설정

--- src/main/kotlin/com/example/nextu/config/SwaggerConfig.kt
+++ src/main/kotlin/com/example/nextu/config/SwaggerConfig.kt
@@ -3,18 +3,44 @@ package com.example.nextu.config
 import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
+/**
+ * https://springdoc.org/
+ */
 @Configuration
 class SwaggerConfig {
+    private val jwtSchemeName = "auth"
+
     @Bean
     fun openApi(): OpenAPI {
-        return OpenAPI().components(Components()).info(Info())
+        // API 요청헤더에 인증 정보 포함
+        return OpenAPI()
+            .addSecurityItem(securityRequirement())
+            .components(components())
+            .info(apiInfo())
     }
 
-    fun apiInfo() = Info()
+    private fun apiInfo() = Info()
         .title("Springdoc 테스트")
         .description("Springdoc 을 활용한 Swagger ui 테스트")
         .version("1.0.0")
+
+    private fun components(): Components {
+        val securityScheme = SecurityScheme()
+            .name(jwtSchemeName)
+            .type(SecurityScheme.Type.HTTP)
+            .scheme("bearer")
+            .bearerFormat("JWT")
+
+        // security schemes 등록
+        return Components()
+            .addSecuritySchemes(jwtSchemeName, securityScheme)
+    }
+
+    // API 요청헤더에 인증 정보 포함
+    private fun securityRequirement() = SecurityRequirement().addList(jwtSchemeName)
 }
